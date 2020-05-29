2708e38aed75473049225145980cd5f527eff931
2224613103@qq.com <2224613103@qq.com>
2020-05-29 11:34:16 +0800

    添加项目文件。

---
+++ BookShopDAL/BaseServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  BookShopIDAL;
+
+
+/*
+ *类名称：BaseServer
+ *类描述：
+ *创建人：ASUS
+ *创建时间：2020-05-29 09:59:12
+ *修改人：ASUS
+ *修改时间：2020-05-29 09:59:12
+ */
+
+namespace BookShopDAL
+{
+    public class BaseServer<T>:IBaseServer<T> where T :class,new()
+    {
+        
+        public async Task<int> InsertAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> QueryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> UpdateAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> DeleteAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> QueryAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookShopIDAL/IBaseServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ *类名称：IBaseServer
+ *类描述：
+ *创建人：ASUS
+ *创建时间：2020-05-29 09:58:40
+ *修改人：ASUS
+ *修改时间：2020-05-29 09:58:40
+ */
+
+namespace BookShopIDAL
+{
+    public interface IBaseServer<T> where T : class
+    {
+        Task<int> InsertAsync(T model);
+        Task<T> QueryAsync(int id);
+        Task<int> UpdateAsync(T model);
+        Task<int> DeleteAsync(T model);
+        IQueryable<T> QueryAll();
+    }
+}
